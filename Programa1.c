80d4139e963772b5eda39751d357f9f51da566f5
Óscar Muñoz <oscarmc99@gmail.com>
2017-12-23 18:29:18 +0100

    Update and rename Programa1 to Programa1.c

---
+++ Programa1.c
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+bool sumaPrimo(int x,int y);
+bool esPrimo(int num);
+int main(){
+	int a,b;
+	
+	cout<<"introduzca dos numeros:"<<endl;
+	cin>>a>>b;
+	
+	if(sumaPrimo(a,b)) cout<<"La suma de los dos numeros es "<<a+b<<" y es primo"<<endl;
+	else cout<<"La suma de los dos numeros es "<<a+b<<" y no es primo"<<endl;
+	
+	return 0;
+}
+
+bool sumaPrimo(int x,int y){
+	if(esPrimo(x+y)){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+bool esPrimo(int num){
+	int contador = 0;
+	for(int i=1;i<(num+1);i++)
+		if(num%i==0) contador++;
+	if(contador!=2) return 0;
+	else return 1;
+}
